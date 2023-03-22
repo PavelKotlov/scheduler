522cdc849aef0e0b80b5840d685b8b46335dcbdc
PavelKotlov <pavelkotlov@gmail.com>
2023-03-22 00:15:49 -0700

    ➕ Jest test to ensure DayListItem renders spots feature accurately

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,12 +3,22 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const formatSpots = spots => {
+    if (spots === 1) {
+      return "1 spot remaining";
+    }
+    
+    return `${spots ? spots : 'no'} spots remaining`;
+  };
+
   const dayClass = classNames('day-list__item', {'day-list__item--selected': props.selected, 'day-list__item--full': !props.spots});
-  
+
+  const remainingSpots = formatSpots(props.spots);
+
   return (
     <li onClick={() => props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{remainingSpots}</h3>
     </li>
   );
 }
