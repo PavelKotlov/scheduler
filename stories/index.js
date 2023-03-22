a2efada5fef468a72b46eaf2064ca6725c1d2493
PavelKotlov <pavelkotlov@gmail.com>
2023-03-22 15:30:55 -0700

    ➕ Form component and edit/create stories to test on Storybook

--- stories/index.js
+++ stories/index.js
@@ -18,6 +18,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 //
 // Create stories for Storybook below
@@ -181,4 +182,18 @@ storiesOf("Appointment", module)
     <Error 
       message="Could not delete appointment."
       onClose={action("onClose")}
-    />);
+    />)
+  .add("Edit", () => 
+    <Form
+      student="Pavel Kotlov"
+      interviewer={3}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />)
+  .add("Create", () => 
+    <Form 
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />)
