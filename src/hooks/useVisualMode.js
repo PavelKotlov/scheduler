3d6025962abbf0d28904306695cc3461595480ed
PavelKotlov <pavelkotlov@gmail.com>
2023-03-24 12:25:20 -0700

    ➕ useVisualMode hook to transition between different appointment modes

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode);
+    
+    replace ?
+    setHistory(prev => [...prev.slice(0, prev.length - 1), newMode]) :
+    setHistory(prev => [...prev, newMode]);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      setMode(history[history.length - 2]);
+      setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+    }
+  }
+
+  return {
+    mode,
+    transition,
+    back
+  };
+}
